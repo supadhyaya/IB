8f8537011d74e57495dfbcef1de6aa1fbfbb1d3b
Sanjiv <sanjiv.upadhyaya@tum.de>
2013-10-27 21:24:43 +0100

    Update config_gapi.php
    removed passwords and user credentials

--- auth/config_gapi.php
+++ auth/config_gapi.php
@@ -3,24 +3,24 @@ require ('/opt/database-migration/current/googleAnalytics_socialNetworks/gapi.cl
 	
 //GAPI Account Login Details 
 	
-	define('ga_email_voycer','idpbiw13@gmail.com');
-	define('ga_password_voycer','my_idpbiw13');
-	define('ga_profile_id_voycer','50665711');
+	define('ga_email_voycer','abc@gmail.com');
+	define('ga_password_voycer','abc');
+	define('ga_profile_id_voycer','65711');
 	
-	define('ga_email_sixx','idpbiw13@gmail.com');
-	define('ga_password_sixx','my_idpbiw13');
-	define('ga_profile_id_sixx','50665711');
+	define('ga_email_sixx','abc@gmail.com');
+	define('ga_password_sixx','abc');
+	define('ga_profile_id_sixx','55711');
 	
-	define('ga_email_fitforfun','idpbiw13@gmail.com');
-	define('ga_password_fitforfun','my_idpbiw13');
-	define('ga_profile_id_fitforfun','50665711');
+	define('ga_email_fitforfun','abc@gmail.com');
+	define('ga_password_fitforfun','abc');
+	define('ga_profile_id_fitforfun','55711');
 	
-	define('ga_email_mmi','idpbiw13@gmail.com');
-	define('ga_password_mmi','my_idpbiw13');
-	define('ga_profile_id_mmi','50665711');
+	define('ga_email_mmi','abc@gmail.com');
+	define('ga_password_mmi','abc');
+	define('ga_profile_id_mmi','55711');
 	
-	define('ga_email_connect','idpbiw13@gmail.com');
-	define('ga_password_connect','my_idpbiw13');
-	define('ga_profile_id_connect','50665711');
+	define('ga_email_connect','abc@gmail.com');
+	define('ga_password_connect','abc');
+	define('ga_profile_id_connect','65711');
 	
 ?>
